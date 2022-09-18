ff983bdbd762cc74e1f8895785d104a89bec8e78
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2022-09-18 19:36:16 +0300

    project init

---
+++ app/src/main/java/com/rokudo/xpense/activities/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.rokudo.xpense.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.rokudo.xpense.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.nav_host_fragment).setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+    }
+}
