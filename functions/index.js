06bdce62ac13221af7db394e281c75f23a9727b3
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2022-10-04 19:53:05 +0300

    begin working on user updates function

--- functions/index.js
+++ functions/index.js
@@ -16,3 +16,49 @@ exports.transactionInsertionListener = functions.firestore.document("Transaction
     }
 
 });
+
+exports.profilePictureChangeListener = functions.firestore.document("Users/{userPhoneNumber}").onWrite((snap, context) => {
+
+    const userPhoneNumber = context.params.userPhoneNumber;
+
+    const documentAfter = snap.after.exists ? snap.after.data() : null;
+    const documentBefore = snap.before.exists ? snap.before.data() : null;
+
+    const picBefore = documentBefore.pictureUrl;
+    const picAfter = documentAfter.pictureUrl;
+    const nameBefore = documentBefore.name;
+    const nameAfter = documentAfter.name;
+
+    const userID = snap.before.uid;
+
+    if (picBefore != picAfter || nameBefore != nameAfter) {
+
+        return admin.firestore().collection("Conversations").where("participants_phone_number_list", "array-contains", userID).get()
+        .then(querySnapshot => {
+
+            querySnapshot.forEach(doc => {
+                let participants_users_list = doc.data().participants_users_list;
+
+                participants_users_list.forEach(element => {
+                    if (element.phoneNumber === userID) {
+                        element.name = nameAfter;
+                        element.pictureUrl = picAfter;
+                        element.token = tokenAfter;
+                    }
+                });
+
+                batch.update(doc.ref, { "participants_users_list": participants_users_list, "updatedByFunctions": true });
+
+            });
+
+        }).then(() => {
+            batch.commit();
+            return console.log("Successfully updated conversations for user " + userID);
+        });
+
+    } else {
+        return console.log("Nothing to change for user");
+    }
+
+
+});
