3255a9cb38d1f4f1489f2c662aeb5a28ab1156a9
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2022-11-02 09:57:05 +0200

    impl invite notification actions

--- app/src/main/java/com/rokudo/xpense/notifications/DeclineReceiver.java
+++ app/src/main/java/com/rokudo/xpense/notifications/DeclineReceiver.java
@@ -1,12 +1,23 @@
 package com.rokudo.xpense.notifications;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.rokudo.xpense.models.Invitation;
+import com.rokudo.xpense.utils.DatabaseUtils;
+
 public class DeclineReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (intent.hasExtra("walletID")) {
+            DatabaseUtils.invitationsRef.document(intent.getStringExtra("walletID"))
+                    .update("status", Invitation.STATUS_DECLINED);
 
+            int notificationID = intent.getIntExtra("notificationID", 0);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancel(notificationID);
+        }
     }
 }
