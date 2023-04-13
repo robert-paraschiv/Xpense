a29bc20b94b5f85313bc1173dd9c63e45dff7773
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2023-04-13 09:13:28 +0300

    add month selection to analytics fragment

--- app/src/main/java/com/rokudo/xpense/fragments/AnalyticsFragment.java
+++ app/src/main/java/com/rokudo/xpense/fragments/AnalyticsFragment.java
@@ -1,13 +1,15 @@
 package com.rokudo.xpense.fragments;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static androidx.recyclerview.widget.RecyclerView.VERTICAL;
-import static com.rokudo.xpense.utils.BarChartUtils.setupBarChart;
 import static com.rokudo.xpense.utils.BarDetailsUtils.setupBarChart;
 import static com.rokudo.xpense.utils.DateUtils.monthYearFormat;
 import static com.rokudo.xpense.utils.PieChartUtils.setupPieChart;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +20,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.transition.TransitionManager;
 
+import com.google.android.material.chip.Chip;
 import com.google.android.material.shape.ShapeAppearanceModel;
+import com.google.android.material.transition.MaterialArcMotion;
 import com.google.android.material.transition.MaterialContainerTransform;
+import com.google.android.material.transition.MaterialSharedAxis;
 import com.rokudo.xpense.R;
 import com.rokudo.xpense.adapters.ExpenseCategoryAdapter;
 import com.rokudo.xpense.data.viewmodels.TransactionViewModel;
@@ -29,14 +36,16 @@ import com.rokudo.xpense.databinding.FragmentAnalyticsBinding;
 import com.rokudo.xpense.models.ExpenseCategory;
 import com.rokudo.xpense.models.Transaction;
 import com.rokudo.xpense.models.Wallet;
-import com.rokudo.xpense.utils.BarChartUtils;
 import com.rokudo.xpense.utils.BarDetailsUtils;
 import com.rokudo.xpense.utils.CategoriesUtil;
 import com.rokudo.xpense.utils.MapUtil;
 import com.rokudo.xpense.utils.PieChartUtils;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -45,10 +54,13 @@ import java.util.Map;
 import java.util.Objects;
 
 public class AnalyticsFragment extends Fragment {
+    private static final String TAG = "AnalyticsFragment";
 
     FragmentAnalyticsBinding binding;
     TransactionViewModel transactionViewModel;
     private Double sum = 0.0;
+    Date selectedDate = new Date();
+
 
     Wallet wallet;
 
@@ -65,6 +77,7 @@ public class AnalyticsFragment extends Fragment {
         transactionViewModel = new ViewModelProvider(requireActivity()).get(TransactionViewModel.class);
 
         initDateChip();
+        buildDatePickerRv();
         getArgsPassed();
         setUpExpenseCategoryRv();
         initOnClicks();
@@ -78,9 +91,46 @@ public class AnalyticsFragment extends Fragment {
     }
 
     private void initOnClicks() {
-        binding.analyticsTypeImage.setOnClickListener(v -> {
-            toggleChartsVisibility();
+        binding.analyticsTypeImage.setOnClickListener(v -> toggleChartsVisibility());
+
+        binding.monthChipGroup.setOnCheckedStateChangeListener((group, checkedIds) -> {
+            Chip chip = group.findViewById(checkedIds.get(0));
+            if (chip != null) {
+                try {
+                    selectedDate = monthYearFormat.parse(chip.getText().toString());
+                    BarDetailsUtils.setBarLabelRotation(binding.barChart, true);
+                    resetCategoriesRv();
+                    loadMonthTransactions(selectedDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
         });
+        binding.dateChip.setOnClickListener(v -> {
+            MaterialSharedAxis materialContainerTransform = new MaterialSharedAxis(MaterialSharedAxis.X, false);
+            materialContainerTransform.setPathMotion(new MaterialArcMotion());
+            materialContainerTransform.setDuration(getResources().getInteger(R.integer.transition_duration_millis));
+
+            BarDetailsUtils.setBarLabelRotation(binding.barChart, true);
+            TransitionManager.beginDelayedTransition(binding.monthLayout, materialContainerTransform);
+
+            binding.dateChipCard.setVisibility(GONE);
+            binding.monthCard.setVisibility(VISIBLE);
+
+            binding.monthHorizontalScroll.postDelayed(() -> {
+                Chip chip = binding.monthChipGroup.findViewWithTag(monthYearFormat.format(selectedDate.getTime()));
+                binding.monthHorizontalScroll.smoothScrollTo(chip.getLeft() - chip.getPaddingLeft(), chip.getTop());
+            }, 30);
+        });
+        binding.backBtn.setOnClickListener(view -> Navigation.findNavController(binding.backBtn).popBackStack());
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void resetCategoriesRv() {
+        categoryList = new ArrayList<>();
+        adapter = new ExpenseCategoryAdapter(categoryList, wallet.getCurrency());
+        binding.categoriesRv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     private void toggleChartsVisibility() {
@@ -177,6 +227,105 @@ public class AnalyticsFragment extends Fragment {
     private void initDateChip() {
         binding.dateChip.setText(monthYearFormat.format(new Date()));
     }
+
+    private void buildDatePickerRv() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        int yearNow = calendar.get(Calendar.YEAR);
+
+        LocalDate localDate = LocalDate.of(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH) + 1,
+                1);
+
+        calendar.set(Calendar.YEAR, 2022);
+        calendar.set(Calendar.MONTH, 0);
+
+        do {
+            for (int i = 0; i < 12; i++) {
+                calendar.set(Calendar.MONTH, i);
+
+                LocalDate innerLocalDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, 1);
+                if (innerLocalDate.isAfter(localDate)) {
+                    break;
+                }
+
+                Chip chip = (Chip) getLayoutInflater().inflate(R.layout.item_month_picker, binding.monthChipGroup, false);
+                chip.setText(monthYearFormat.format(calendar.getTime()));
+                chip.setTag(monthYearFormat.format(calendar.getTime()));
+                if (monthYearFormat.format(calendar.getTime()).equals(monthYearFormat.format(new Date()))) {
+                    chip.setChecked(true);
+                }
+                binding.monthChipGroup.addView(chip);
+            }
+
+            calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + 1);
+
+        } while (calendar.get(Calendar.YEAR) <= yearNow);
+    }
+
+    private void loadMonthTransactions(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.getActualMaximum(Calendar.DAY_OF_MONTH),
+                23, 59);
+        Date end = calendar.getTime();
+
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.getActualMinimum(Calendar.DAY_OF_MONTH),
+                0, 0);
+        Date start = calendar.getTime();
+
+        binding.dateChip.setText(monthYearFormat.format(start));
+        if (monthYearFormat.format(start).equals(monthYearFormat.format(new Date()))) {
+            binding.periodCard.setVisibility(VISIBLE);
+            binding.allMonthChip.setChecked(true);
+        } else {
+            binding.periodCard.setVisibility(GONE);
+        }
+
+        MaterialSharedAxis materialContainerTransform = new MaterialSharedAxis(MaterialSharedAxis.X, true);
+        materialContainerTransform.setPathMotion(new MaterialArcMotion());
+        materialContainerTransform.setDuration(getResources().getInteger(R.integer.transition_duration_millis));
+
+        TransitionManager.beginDelayedTransition(binding.monthLayout, materialContainerTransform);
+
+        binding.monthCard.setVisibility(GONE);
+        binding.dateChipCard.setVisibility(VISIBLE);
+
+        loadTransactions(start, end, true, false);
+    }
+
+    private void loadTransactions(Date start, Date end, Boolean updateBar, Boolean last7Days) {
+        transactionViewModel
+                .loadTransactionsDateInterval(wallet.getId(), start, end)
+                .observe(getViewLifecycleOwner(), values -> {
+                    if (values == null || values.isEmpty()) {
+                        Log.e(TAG, "loadTransactions: empty");
+                        BarDetailsUtils.updateBarchartData(binding.barChart,
+                                new ArrayList<>(),
+                                new TextView(requireContext()).getCurrentTextColor(), last7Days);
+                        binding.totalAmountTv.setText(R.string.no_data_provided);
+                    } else {
+                        sortTransactions(values);
+                        if (updateBar) {
+                            BarDetailsUtils.updateBarchartData(binding.barChart,
+                                    values,
+                                    new TextView(requireContext()).getCurrentTextColor(), last7Days);
+//                            transEntryList = BarDetailsUtils.getTransEntryArrayList(values, last7Days);
+                        }
+//                        if (firstLoad) {
+//                            startPostponedEnterTransition();
+//                            firstLoad = false;
+//                        }
+                        binding.categoriesRv.scheduleLayoutAnimation();
+                    }
+                });
+    }
+
 
     private void getArgsPassed() {
         AnalyticsFragmentArgs args = AnalyticsFragmentArgs.fromBundle(requireArguments());
