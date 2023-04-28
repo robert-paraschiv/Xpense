19ca5534317983973993b8df317e7d42db5f057d
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2023-04-28 11:08:55 +0300

    updated categories colours

--- app/src/main/java/com/rokudo/xpense/utils/CategoriesUtil.java
+++ app/src/main/java/com/rokudo/xpense/utils/CategoriesUtil.java
@@ -15,43 +15,43 @@ public class CategoriesUtil {
         expenseCategoryList = new ArrayList<>();
         expenseCategoryList.add(new ExpenseCategory("Groceries",
                 R.drawable.ic_baseline_local_grocery_store_24,
-                rgb("#C0FF8C")));
+                rgb("#B2FF59")));
         expenseCategoryList.add(new ExpenseCategory("Restaurant",
                 R.drawable.ic_baseline_restaurant_24,
-                rgb("#FFF78C")));
+                rgb("#F50057")));
         expenseCategoryList.add(new ExpenseCategory("Drinks",
                 R.drawable.ic_round_wine_bar_24,
-                rgb("#FFD08C")));
+                rgb("#F06292")));
         expenseCategoryList.add(new ExpenseCategory("Transport",
                 R.drawable.ic_round_directions_car_24,
-                rgb("#8CEAFF")));
+                rgb("#18FFFF")));
         expenseCategoryList.add(new ExpenseCategory("Fuel",
                 R.drawable.baseline_local_gas_station_24,
-                rgb("#80CBC4")));
+                rgb("#37474F")));
         expenseCategoryList.add(new ExpenseCategory("Bills",
                 R.drawable.ic_round_receipt_24,
-                rgb("#FF8C9D")));
+                rgb("#AD1457")));
         expenseCategoryList.add(new ExpenseCategory("Gifts",
                 R.drawable.ic_round_card_giftcard_24,
-                rgb("#2ecc71")));
+                rgb("#FFFF00")));
         expenseCategoryList.add(new ExpenseCategory("Medical",
                 R.drawable.ic_baseline_medication_24,
-                rgb("#f1c40f")));
+                rgb("#448AFF")));
         expenseCategoryList.add(new ExpenseCategory("Others",
                 R.drawable.ic_round_category_24,
-                rgb("#e74c3c")));
+                rgb("#FF6D00")));
 
         expenseCategoryList.add(new ExpenseCategory("Housing",
                 R.drawable.baseline_house_24,
-                rgb("#4E342E")));
+                rgb("#4527A0")));
 
         expenseCategoryList.add(new ExpenseCategory("Clothing",
                 R.drawable.baseline_shopping_bag_24,
-                rgb("#D500F9")));
+                rgb("#9575CD")));
 
         expenseCategoryList.add(new ExpenseCategory("Entertainment",
                 R.drawable.baseline_theater_comedy_24,
-                rgb("#F57F17")));
+                rgb("#69F0AE")));
 
         expenseCategoryList.add(new ExpenseCategory("Income",
                 R.drawable.round_monetization_on_24,
