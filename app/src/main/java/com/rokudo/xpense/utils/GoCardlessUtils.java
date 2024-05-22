d375274b13d3bb8b95ce839ac7950b3d89777c13
Robert Paraschiv <35243268+robert-paraschiv@users.noreply.github.com>
2024-05-22 20:55:25 +0300

    a key

--- app/src/main/java/com/rokudo/xpense/utils/GoCardlessUtils.java
+++ app/src/main/java/com/rokudo/xpense/utils/GoCardlessUtils.java
@@ -5,9 +5,9 @@ public class GoCardlessUtils {
 
     public static final String TOKEN_PREFS_NAME = "token";
 
-    public static String GOCARDLESS_SECRET_KEY_ID = "eea4ce2c-cf75-4cd3-816c-d54b14faf54f";
+    public static String GOCARDLESS_SECRET_KEY_ID = "dfad5a53-8c4c-48a0-91cb-2a4340f9d4a6";
 
     public static String GOCARDLESS_SECRET_KEY =
-            "0c45859da091784a40709fbebee541614b09686591d4accb33830f4100bbe0101fb168a762b13b7bb0a4614f0afe7220aae4a7d20029f5bfc4d03033cf07092e";
+            "99d4e5045c2cb098bc12897110b886510e4b8a36cdb0647d6866bcc8df4cabd75b20f80204df74e596aa56fbafdf39abd414dfbf24578aae59bf7cd0126b7d31";
 
 }
